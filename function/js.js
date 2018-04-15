f4b07e133c0f8fb187a64e14f3b6320cfea13cd9
aa11ac <ach.aayush.111@gmail.com>
2018-04-15 14:31:53 +0545

    update

--- function/js.js
+++ function/js.js
@@ -6,6 +6,10 @@ $(document).ready(function(){
   var inputString;
 
   // stores thae value without "."
-  var operators1=[""];
+  var operators1=["+","-","/","*"];
+  // operator with "."
+  var operator2=["."];
+  // numbers for validation
+  var nums=[0,1,2,3,4,5,6,7,8,9];
   
 })
