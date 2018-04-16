b4cbc68d8866b50a4dbb8543209a54beaa5044d1
aa11ac <ach.aayush.111@gmail.com>
2018-04-16 09:48:44 +0545

    update

--- function/js.js
+++ function/js.js
@@ -11,5 +11,39 @@ $(document).ready(function(){
   var operator2=["."];
   // numbers for validation
   var nums=[0,1,2,3,4,5,6,7,8,9];
+
+  function getValue(input){
+
+
+  }
+
+  function update(){
+    totalString=inputs.join("");
+   $(".input").html(totalString);
+
+  }
+
+  function getTotal(){
+    totalString=inputs.join("");
+    $(".output").html(eval(totalString));
+  }
   
+
+
+  $("button").on("click",function(){
+    if (this.value==="AC"){
+       console.log("clickedac");
+      inputs=[""];
+      update();
+    }
+    else if(this.value==="CE"){
+      console.log("ce");
+      inputs.pop();
+      update();
+    }
+    else if(this.value==="="){
+      console.log("=");
+      getTotal();
+    }
+  })
 })
